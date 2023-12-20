e83718583cf25488dde1dbd8b54ccfae4ad53d33
EduBGou <136281205+EduBGou@users.noreply.github.com>
2023-12-20 15:15:27 -0300

    finished

--- scripts/script.js
+++ scripts/script.js
@@ -3,10 +3,12 @@ const bxsLowOpacity = document.querySelectorAll('.bxAnimalsLowOpacity')
 bxsLowOpacity.forEach(bx => {
     let bttn
     bx.addEventListener('mouseenter', () => {
-        if (!bttn){bttn = document.createElement('button')
-            bttn.innerHTML = '<p>Quero Adotar</p> <img class="Icon" id="iconWhatsapp" src="../images/iconWhatsapp.png" alt="">'
+        if (!bttn){
+            bttn = document.createElement('button')
+            bttn.innerHTML = '<p>Quero Adotar</p> <img class="Icon" id="iconWhatsapp" src="../images/iconWhatsapp.png" alt="WhatsApp">'
             bttn.id = 'bttnAdoption'
             bx.appendChild(bttn)
+            
         }
     })
     bx.addEventListener('mouseleave', () => {
