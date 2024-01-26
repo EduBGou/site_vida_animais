abeb4beddb84553eac105d19094cdc3efc6d22ed
EduBGou <136281205+EduBGou@users.noreply.github.com>
2024-01-25 21:00:59 -0300

    Update script.js

--- scripts/script.js
+++ scripts/script.js
@@ -3,16 +3,17 @@ const bxsLowOpacity = document.querySelectorAll('.bxAnimalsLowOpacity')
 bxsLowOpacity.forEach(bx => {
     let bttn
     bx.addEventListener('mouseenter', () => {
-        if (!bttn){
+        if (!bttn)
+        {
             bttn = document.createElement('button')
             bttn.innerHTML = '<p>Quero Adotar</p> <img class="Icon" id="iconWhatsapp" src="../images/iconWhatsapp.png" alt="WhatsApp">'
             bttn.id = 'bttnAdoption'
-            bx.appendChild(bttn)
-            
+            bx.appendChild(bttn) 
         }
     })
     bx.addEventListener('mouseleave', () => {
-        if (bttn){
+        if (bttn)
+        {
             bttn.remove()
             bttn = null
         }
